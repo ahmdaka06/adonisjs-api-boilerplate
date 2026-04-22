e211971b740b107d0d1c62599c46d66966e31424
Ahmad Andika <ahmdaka06@gmail.com>
2026-04-23 00:28:01 +0700

    refactor(api): use AdonisJS built-in transformers for auth and RBAC responses

--- .adonisjs/server/controllers.ts
+++ .adonisjs/server/controllers.ts
@@ -5,4 +5,5 @@
 
 export const controllers = {
   Auth: () => import('#controllers/auth_controller'),
+  Profile: () => import('#controllers/profile_controller'),
 }
--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -4,10 +4,11 @@ import { loginValidator } from '#validators/auth/login'
 import { registerValidator } from '#validators/auth/register';
 import Role from '#models/role';
 import { RoleEnum } from '../../shared/enums/role_enum.ts';
+import UserTransformer from '#transformers/user_transformer';
 
 export default class AuthController {
 
-    async register({request, response, auth}: HttpContext) {
+    async register({request, response, serialize, auth}: HttpContext) {
         const payload = await request.validateUsing(registerValidator)
 
         const existingUser = await User.findBy('email', payload.email)
@@ -42,35 +43,24 @@ export default class AuthController {
 
         const token = await auth.use('api').createToken(freshUser)
 
-        const permissions = [
+        /* const permissions = [
             ...new Set(
                 freshUser.roles.flatMap((role) =>
                     role.permissions.map((permission) => permission.slug)
                 )
             ),
-        ]
+        ] */
 
         return response.created({
             message: 'Register successful',
             data: {
                 token: token.value!.release(),
-                user: {
-                    id: freshUser.id,
-                    full_name: freshUser.fullName,
-                    email: freshUser.email,
-                    is_active: freshUser.isActive,
-                    roles: freshUser.roles.map((role) => ({
-                        id: role.id,
-                        name: role.name,
-                        slug: role.slug,
-                    })),
-                    permissions,
-                },
+                user:serialize(UserTransformer.transform(freshUser)),
             },
         })
     }
 
-    async login({request, response}: HttpContext) {
+    async login({request, response, serialize}: HttpContext) {
         const payload = await request.validateUsing(loginValidator)
 
         const user = await User.verifyCredentials(payload.email, payload.password)
@@ -89,35 +79,24 @@ export default class AuthController {
             name: 'api-login'
         })
 
-        const permissions = [
-            ... new Set(
-                user.roles.flatMap((role) =>
+        /* const permissions = [
+            ...new Set(
+                freshUser.roles.flatMap((role) =>
                     role.permissions.map((permission) => permission.slug)
                 )
-            )
-        ]
+            ),
+        ] */
 
         return response.ok({
             message: 'Login successful',
             data: {
                 token: token.value!.release(),
-                user: {
-                    id: user.id,
-                    full_name: user.fullName,
-                    email: user.email,
-                    is_active: user.isActive,
-                    roles: user.roles.map((role) => ({
-                        id: role.id,
-                        name: role.name,
-                        slug: role.slug,
-                    })),
-                    permissions,
-                },
+                user: serialize(UserTransformer.transform(user)),
             },
         })
     }
 
-    async me({ auth, response }: HttpContext) {
+    async me({ auth, response, serialize }: HttpContext) {
         const authUser = auth.user!
 
         const user = await User.query()
@@ -127,31 +106,18 @@ export default class AuthController {
             })
             .firstOrFail()
 
-        const permissions = [
+        /* const permissions = [
             ...new Set(
-                user.roles.flatMap((role) =>
+                freshUser.roles.flatMap((role) =>
                     role.permissions.map((permission) => permission.slug)
                 )
             ),
-        ]
+        ] */
 
 
         return response.ok({
             message: 'Profile fetched successfully',
-            data: {
-                user: {
-                    id: user.id,
-                    full_name: user.fullName,
-                    email: user.email,
-                    is_active: user.isActive,
-                    roles: user.roles.map((role) => ({
-                        id: role.id,
-                        name: role.name,
-                        slug: role.slug,
-                    })),
-                    permissions,
-                },
-            },
+            data: serialize(UserTransformer.transform(user)),
         })
     }
 
